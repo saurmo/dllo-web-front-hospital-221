df210a6dfaafe7fd5c29a4a9dfcb7ef7a62e09cc
pea0715 <pabloespinosarias11c@gmail.com>
2022-04-20 17:46:42 -0500

    actualizacion de pagina desarrollador

---
+++ pages/desarrolladores/pablo-espinosa.vue
@@ -0,0 +1,24 @@
+<template>
+    <b-container>
+        <h1>PEA</h1>
+        <center>
+            <div>
+            <b-card
+                title="Card Title"
+                img-src="https://picsum.photos/600/300/?image=25"
+                img-alt="Image"
+                img-top
+                tag="article"
+                style="max-width: 20rem;"
+                class="mb-2"
+                >
+                <b-card-text>
+                Some quick example text to build on the card title and make up the bulk of the card's content.
+                </b-card-text>
+
+                <b-button href="mailto:pabloespinosarias11c@gmail.com" variant="primary">pabloespinosarias11c@gmail.com</b-button>
+            </b-card>
+            </div>
+        </center>
+    </b-container>
+</template>
