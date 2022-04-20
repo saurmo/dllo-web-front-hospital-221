ed30b39ec053ad30d100db126fa64b186ba56af6
Juan Diego Rodríguez Guarín <minionpaul85@gmail.com>
2022-04-20 17:40:35 -0500

    - Actualización página desarrollador

---
+++ pages/desarrolladores/juandi-rodriguez.vue
@@ -0,0 +1,25 @@
+<template>
+  <b-container>
+    <h1>Juan Diego Rodríguez Guarín</h1>
+    <center>
+      <div>
+        <b-card
+          title="Juandirg"
+          img-src="https://picsum.photos/600/300/?image=25"
+          img-alt="Image"
+          img-top
+          tag="article"
+          style="max-width: 20rem"
+          class="mb-2"
+        >
+          <b-card-text>
+            Just a simple guy trying to learn programming but I always ended up
+            wasting my time playing videogames XD
+          </b-card-text>
+
+          <b-button href="#" variant="primary">juandirg90@gmail.com</b-button>
+        </b-card>
+      </div>
+    </center>
+  </b-container>
+</template>
