ed70cb1354f29b9793ee4231665cb169c71ff95d
EquirozVasco <esteban.quirozv@gmail.com>
2022-04-20 17:52:37 -0500

    modificación nombre archivo esteban-quiroz

---
+++ pages/desarrolladores/esteban-quiroz.vue
@@ -0,0 +1,26 @@
+<template>
+  <b-container>
+    <center>
+      <h1>
+        Esteban Quiroz Vasco
+      </h1>
+      <div>
+        <b-card
+          title="Card Title"
+          img-src="https://picsum.photos/600/300/?image=25"
+          img-alt="Image"
+          img-top
+          tag="article"
+          style="max-width: 20rem"
+          class="mb-2"
+        >
+          <b-card-text>
+            Somewhere over the rainbow.
+          </b-card-text>
+
+          <b-button href="#" variant="primary">esteban.quirozv@gmail.com</b-button>
+        </b-card>
+      </div>
+    </center>
+  </b-container>
+</template>
