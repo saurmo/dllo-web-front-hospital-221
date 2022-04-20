215e48a00b014d3db55013a5641d7cc5d4262611
dileyson <dem4.echeverria@gmail.com>
2022-04-20 17:40:01 -0500

    actualizacion pagina presentacion -- creaciòn rama

---
+++ pages/desarrolladores/dileyson.vue
@@ -0,0 +1,24 @@
+<template>
+  <b-container>
+    <center>
+      <h1>Dileyson</h1>
+      <div>
+        <b-card
+          title="Dileyson Echeverria"
+          img-src="https://picsum.photos/600/300/?image=25"
+          img-alt="Image"
+          img-top
+          tag="article"
+          style="max-width: 20rem"
+          class="mb-2"
+        >
+          <b-card-text>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe nihil tempora a error eius. Fugiat quia modi iusto aliquam aut, aliquid maxime eligendi perferendis, debitis vero, asperiores quibusdam cumque. Animi?
+          </b-card-text>
+
+          <b-button href="mailto:dem4.echeverria@gmail.com" variant="primary">dem4.echeverria@gmail.com</b-button>
+        </b-card>
+      </div>
+    </center>
+  </b-container>
+</template>
