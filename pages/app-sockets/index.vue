7e04b3978d3601dcdabde7c5f9f2fd655a019c5e
saurmo0108@gmail.com <saurmo0108@gmail.com>
2022-05-20 18:47:52 -0500

    - page sockets

---
+++ pages/app-sockets/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(user, index) in users" :key="index">
+        {{ user }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+import { io } from "socket.io-client";
+export default {
+  beforeMount() {
+    this.loadPage();
+  },
+  data() {
+    return {
+      users: [],
+      socket:null
+    };
+  },
+  methods: {
+    loadPage() {
+      console.log(io);
+      
+      this.socket=io()
+    },
+  },
+};
+</script>
